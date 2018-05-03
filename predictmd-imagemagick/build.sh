bd8e12d7568cc9995bc014f1b8fc9f140955944a
Dilum Aluthge <dilum@aluthge.com>
2018-05-03 17:10:19 -0400

    Initial commit

---
+++ predictmd-imagemagick/build.sh
@@ -0,0 +1,17 @@
+set -o verbose
+set -o errexit
+
+#wget --output-document=ImageMagick.tar.gz https://www.imagemagick.org/download/ImageMagick-6.9.9-42.tar.gz
+cp $RECIPE_DIR/downloads/ImageMagick-6.9.9-42.tar.gz .
+mv ImageMagick-6.9.9-42.tar.gz ImageMagick.tar.gz
+tar -vzxf ImageMagick.tar.gz
+rm -rf ImageMagick.tar.gz
+mv ImageMagick-* ImageMagick
+cd ImageMagick
+./configure --prefix=$PREFIX
+make -j8
+make install
+cd ..
+rm -rf ImageMagick
+
+export MAGICK_HOME=$PREFIX
