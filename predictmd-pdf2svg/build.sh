f3bb65ed2e176df4a1fac76fa72f353cf33a3452
Dilum Aluthge <dilum@aluthge.com>
2018-05-03 22:34:22 -0400

    Change "dependencies" to "downloads"

--- predictmd-pdf2svg/build.sh
+++ predictmd-pdf2svg/build.sh
@@ -2,7 +2,7 @@ set -o verbose
 set -o errexit
 
 #wget --output-document=pdf2svg.tar.gz https://github.com/dawbarton/pdf2svg/archive/v0.2.3.tar.gz
-cp $RECIPE_DIR/dependencies/pdf2svg-0.2.3.tar.gz .
+cp $RECIPE_DIR/downloads/pdf2svg-0.2.3.tar.gz .
 mv pdf2svg-0.2.3.tar.gz pdf2svg.tar.gz
 tar -vzxf pdf2svg.tar.gz
 rm -f pdf2svg.tar.gz
