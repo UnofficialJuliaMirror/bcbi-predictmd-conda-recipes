28f279e80653af57de9eb881b8f01c90ad643f18
Dilum Aluthge <dilum@aluthge.com>
2018-05-26 07:35:12 -0400

    Fix flags

--- predictmd-imagemagick/build.sh
+++ predictmd-imagemagick/build.sh
@@ -8,7 +8,7 @@ tar -vzxf ImageMagick.tar.gz
 rm -rf ImageMagick.tar.gz
 mv ImageMagick-* ImageMagick
 cd ImageMagick
-./configure X_PRE_LIBS="-luuid -lSM -lICE"
+./configure X_PRE_LIBS="-luuid"
 make -j8
 make -j8 install
 cd ..
