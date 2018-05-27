30b718c43c48d9ec9d29bbf56e69ef71ead8df63
Dilum Aluthge <dilum@aluthge.com>
2018-05-26 21:16:55 -0400

    Another attempt to fix imageMagick

--- predictmd-imagemagick/build.sh
+++ predictmd-imagemagick/build.sh
@@ -8,7 +8,7 @@ tar -vzxf ImageMagick.tar.gz
 rm -rf ImageMagick.tar.gz
 mv ImageMagick-* ImageMagick
 cd ImageMagick
-./configure X_PRE_LIBS="-luuid" LDFLAGS="-luuid" LIBS="-luuid" --prefix=$PREFIX
+./configure --prefix=$PREFIX --without-x
 make
 make install
 cd ..
